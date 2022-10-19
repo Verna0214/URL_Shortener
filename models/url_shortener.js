d1d56913fbaa8920fa9f9f7cee67eebdc1783dc1
Verna0214 <yuhsichen0214@gmail.com>
2022-10-19 11:04:38 +0800

    feat: add mongoose schema file

---
+++ models/url_shortener.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const urlshortenerSchema = new Schema({
+  origin_url: {
+    type: String,
+    required: true
+  },
+  shorten_url: {
+    type: String,
+    required: true
+  },
+  shortenCode: {
+    type: String,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('URL_shortener', urlshortenerSchema)
