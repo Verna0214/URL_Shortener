6217e3a81d440fba9627b0a3fb483dd20138287e
Verna0214 <yuhsichen0214@gmail.com>
2022-10-20 22:39:51 +0800

    feat: add params function

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser')
 const URL_shortener = require('./models/url_shortener')
 const generateCode = require('./utilities/generateCode')
 
+const mainUrl = 'http://localhost:'
+const port = 3000
 
 const app = express()
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: 'hbs' }))
@@ -27,22 +29,35 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+app.get('/:shorten', (req, res) => {
+  const shortenCode = req.params.shorten
+  URL_shortener.find({})
+    .lean()
+    .then((urlList) => {
+      url = urlList.find((eachUrl) => eachUrl.shorten_url === `${mainUrl}${port}/${shortenCode}`)
+      if (url) {
+        console.log(url)
+        return res.redirect(url.origin_url)
+      }
+    })
+    .catch(error => console.log(error))
+})
+
 app.post('/', (req, res) => {
   const newUrl = req.body.url
   let urlShorten = ''
   URL_shortener.find()
     .lean()
     .then((urlList) => {
-      urlShorten = urlList.find((eachUrl) => eachUrl.origin_url === newUrl) //urlShorten = eachUrl
+      urlShorten = urlList.find((eachUrl) => eachUrl.origin_url === newUrl)
       if (urlShorten) {
         urlShorten = urlShorten.shorten_url
         return res.render('show', { urlShorten })
       }
 
-      
-    urlShorten = `http://localhost:3000/${generateCode()}`
+      urlShorten = `${mainUrl}${port}/${generateCode()}`
       while (urlList.some((eachUrl) => eachUrl.shorten_url === urlShorten)) {
-        urlShorten = `http://localhost:3000/${generateCode()}` 
+        urlShorten = `${mainUrl}${port}/${generateCode()}`
       }
 
       return URL_shortener.create({
@@ -55,5 +70,5 @@ app.post('/', (req, res) => {
 })
 
 app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000')
+  console.log(`App is running on ${mainUrl}${port}`)
 })
