22eec75132555760ea0366a94aa40f05f40228db
Verna0214 <yuhsichen0214@gmail.com>
2023-08-13 19:14:23 +0800

    feat: add redirect origin url router

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const generateShortURL = require('../utilities/generateShortURL')
 const URL = require('../models/url')
 
+// create url
 router.post('/', (req, res) => {
   if (!req.body.url) return res.redirect('/')
 
@@ -16,7 +17,17 @@ router.post('/', (req, res) => {
         return URL.create({ origin_url: req.body.url, shorten_url: shortURL });
       }
     })
-    .then((data) => res.render('index', { origin: req.headers.origin, shortURL: data.shorten_url }))
+    .then((data) => res.render('shorturl', { origin: req.headers.origin, shortURL: data.shorten_url }))
+    .catch(error => console.log(error))
+})
+
+// copy short url and redirect origin url
+router.get('/:shortURL', (req, res) => {
+  const shortURL = req.params.shortURL
+
+  return URL.findOne({ shorten_url: shortURL })
+    .lean()
+    .then((data) => res.redirect(data.origin_url))
     .catch(error => console.log(error))
 })
 
