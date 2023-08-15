6ca538f90a763586bf8c44552cfa0eac50018888
Verna0214 <yuhsichen0214@gmail.com>
2023-08-15 14:28:58 +0800

    fix: post router about userId

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,7 @@ router.post('/', authenticator, (req, res) => {
       if (data) {
         return data;
       } else {
-        return URL.create({ origin_url: req.body.url, shorten_url: shortURL });
+        return URL.create({ origin_url: req.body.url, shorten_url: shortURL, userId: req.user._id });
       }
     })
     .then((data) => res.render('shorturl', { origin: req.headers.origin, shortURL: data.shorten_url }))
@@ -42,7 +42,13 @@ router.get('/:shortURL', (req, res) => {
 
   return URL.findOne({ shorten_url: shortURL })
     .lean()
-    .then((data) => res.redirect(data.origin_url))
+    .then((data) => {
+      if (data && data.origin_url) {
+        res.redirect(data.origin_url);
+      } else {
+        res.status(404).send('URL not found');
+      }
+    })
     .catch(error => console.log(error))
 })
 
